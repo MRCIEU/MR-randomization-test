001e9da19a51cf8bb2156c9f474730cc65a403c6
Louise Millard <louise.millard@bristol.ac.uk>
2021-02-09 14:20:50 +0000

    add initial sims to assess impact of number of covariates and correlation between them

---
+++ 1-sims/j-sim-corrs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -l walltime=4:00:00,nodes=1:ppn=1
+#PBS -o out-sim-corrs.file
+#---------------------------------------------
+
+date
+
+
+
+####PBS -t 1-10
+
+
+cd $PBS_O_WORKDIR
+
+module add languages/R-4.0.3-gcc9.1.0
+
+#ix=${PBS_ARRAYID}
+#ixStart=$((ix*100-99))
+#ixEnd=$((ixStart+100-1))
+
+
+export RES_DIR="${HOME}/2021-randomization-test/results"
+
+Rscript sim-branson-corrs.R 
+
+date
+
+
+
+
