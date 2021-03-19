d9d9255efbcf66965d44cb4ad1e517985a01f908
Louise Millard <louise.millard@bristol.ac.uk>
2021-03-19 16:29:20 +0000

    refactor selection sim code to separate out data generation

--- 1-sims/b-selection-bias/doSimSelection.R
+++ 1-sims/b-selection-bias/doSimSelection.R
@@ -13,117 +13,46 @@
 # corrC: correlation between covariates
 doSimSelection <- function(nc=100, ncs=100, corrC=0, totalEffectCovarsSelection=10) {
 
+ 
+  print('-------------------')
+  print(paste0("Number of covariates that affect selection: ", ncs, " of ", nc))
+  print(paste0("Correlation between covariates: ", corrC))
+
+
   ###
   ### load packages
 
   # use MASS for mvrnorm and polyr functions
-  library('MASS')
+  #library('MASS')
 
   # lmtest for coeftest
-  library('lmtest')
+  #library('lmtest')
 
   # ivmodel for Branson Mahalanobis distance test
-  library('ivmodel')
+  #library('ivmodel')
 
 
+  ##
+  ## generate sim data
 
   ## z is a snp dosage IV with 3 levels
   ## x is a binary exposure
   ## y is a continuous outcome
 
-  ### is the p value correct when there are dependencies between the covariates
-
   # number in sample
   n = 350000
+  source('generateSimData.R')
+  simdata = generateSimData(n, nc, ncs, corrC, totalEffectCovarsSelection)
 
-  
-  print('-------------------')
-  print(paste0("Number of covariates that affect selection: ", ncs, " of ", nc))
-  print(paste0("Correlation between covariates: ", corrC))
-  
-
-  ###
-  ### generate data
-  
-  ## generate covariates C with particular correlation corrC  
-  corrCMat = diag(nc)
-  corrCMat[which(corrCMat == 0)] = corrC
-  dfC = mvrnorm(n=n, mu=rep(0, nc), Sigma=corrCMat, empirical=FALSE)
-  dfC = as.data.frame(dfC)
-  
-  ## generate a IV with 3 categories
-  ## use p(A1)=0.8, p(A2)=0.2, dosage probs assuming HWE = (0.8^2, 2*0.8*0.2, 0.2^2) = (0.64,0.32,0.04) 
-  ## TODO update this to use these freqs ***************************
-  z = sample(1:3, n, replace=TRUE, prob=c(0.64, 0.32, 0.04))
-  
-
-  ###
-  ### calculate effects of covariates on X and Y, fixing the total effect of C_s and C_nots respectively.
-
-  betaCs_onX = log(2^(1/ncs))
-  betaCnots_onX = log(2^(1/(nc-ncs)))
-
-
-  ###
-  ### generate exposure x
-
-  logitPart = z + rowSums(betaCs_onX*dfC[,1:ncs, drop=FALSE]) + rowSums(betaCnots_onX*dfC[,(ncs+1):nc, drop=FALSE]) 
-  pX = exp(logitPart)/(1+exp(logitPart))
-  x = rep(0, 1, n)
-  x[runif(n) <= pX] = 1
-  
-
-  ###
-  ### generate continuous outcome y
-  # C AND X ARE DETERMINANTS OF Y
-
-  betaCs_onY = 2/ncs
-  betaCnots_onY = 2/(nc-ncs)
-
-  y = rowSums(betaCs_onY*dfC[,1:ncs, drop=FALSE]) + rowSums(betaCnots_onY*dfC[,(ncs+1):nc, drop=FALSE]) + 1*x + rnorm(n,0,1)
-
-
-
-  ###
-  ### generate selection variable s
-
-  # beta is set so that the total effect across all covariates affect d is constant
-  betaC = log(totalEffectCovarsSelection^(1/ncs))
-
-  # X AND (A SUBSET OF) C ARE DETERMINANTS OF SELECTION
-  logitPart = log(2)*x + rowSums(dfC[,1:ncs, drop=FALSE]*betaC) 
-  pS = exp(logitPart)/(1+exp(logitPart))
-  s = rep(0, 1, n)
-  s[runif(n) <= pS] = 1
-
-  # check variance of selection explained by Xs
-  #mylogit <- glm(s ~ ., data=dfC[,1:ncs], family="binomial")
-
-  # check selection variable has same prop'n variance explained by C and same distribution
-#  library('rsq')
-#  rsq(mylogit)
-
-#  table(s)
-  summary(s)
-
-
-  ###
-  ### select subsample
-  
-  z = z[which(s ==1)]
-  dfC = dfC[which(s==1),]
-
-
-
-  
+    
   
   ###
   ### calculate test statistic - Mahalanobis distance
 
   # computationally efficient to generate once rather than for each permutation
-  covDFC = solve(as.matrix(stats::cov(dfC)))
+  covDFC = solve(as.matrix(stats::cov(simdata$dfC)))
   
-  t = as.numeric(getMD3CatsCorr(dfC, z, covDFC))
+  t = as.numeric(getMD3CatsCorr(simdata$dfC, simdata$z, covDFC))
 
 
 
@@ -133,10 +62,9 @@ doSimSelection <- function(nc=100, ncs=100, corrC=0, totalEffectCovarsSelection=
   individualPvalues = c()
   for (i in 1:nc) {
 
-    sumx = summary(lm(dfC[,i] ~ z))
-#    print(sumx)
+    sumx = summary(lm(simdata$dfC[,i] ~ simdata$z))
 
-    pvalue = sumx$coefficients['z','Pr(>|t|)']
+    pvalue = sumx$coefficients['simdata$z','Pr(>|t|)']
     print(paste0('i: ', pvalue, ', adjusted:', pvalue*nc))
 
     individualPvalues = c(individualPvalues, pvalue)
@@ -158,10 +86,10 @@ doSimSelection <- function(nc=100, ncs=100, corrC=0, totalEffectCovarsSelection=
   for (i in 1:nPerms) {
     
     # randomly permute z
-    zperm = sample(z, length(z), replace=FALSE)
+    zperm = sample(simdata$z, length(simdata$z), replace=FALSE)
     
     # calculate test statistic on permuted data
-    testStatPerm = getMD3CatsCorr(dfC, zperm, covX.inv=covDFC)
+    testStatPerm = getMD3CatsCorr(simdata$dfC, zperm, covX.inv=covDFC)
     
     # add test stat of permutated data to our empirial distribution of test statistics
     permTestStats = c(permTestStats, testStatPerm)
@@ -169,6 +97,7 @@ doSimSelection <- function(nc=100, ncs=100, corrC=0, totalEffectCovarsSelection=
     if (i%%100==0) print(i)
 
   }
+  
   
   ###
   ### summarise permutation testing null distribution of test statistics
@@ -178,14 +107,10 @@ doSimSelection <- function(nc=100, ncs=100, corrC=0, totalEffectCovarsSelection=
   print('Summary of null distribution of test statistics (generated with permutation testing):')
   summary(permTestStats)
   
- # print(paste0("0.95 quantile: ", quantile(permTestStats, 0.95) ))
- # print(paste0("max perm value: ", max(permTestStats) ))
-  
   # generate p value
   pvalue = length(which(permTestStats>=t))/nPerms
   print(paste0("Permutation P value: ", pvalue))
   
-
   return(c(pvalue, individualPvalues, indtReject))
 
 }
---
+++ 1-sims/b-selection-bias/generateSimData.R
@@ -0,0 +1,107 @@
+
+
+
+
+# nc: the number of covariates included as candidates
+# ncs: the number of covariates that affect selection
+# corrC: correlation between covariates
+generateSimData <- function(n, nc, ncs, corrC, totalEffectCovarsSelection) {
+
+  ###
+  ### load packages
+
+  # use MASS for mvrnorm and polyr functions
+  library('MASS')
+
+  # lmtest for coeftest
+  #library('lmtest')
+
+  # ivmodel for Branson Mahalanobis distance test
+  #library('ivmodel')
+
+
+
+  ## z is a snp dosage IV with 3 levels
+  ## x is a binary exposure
+  ## y is a continuous outcome
+
+  ### is the p value correct when there are dependencies between the covariates
+
+  # number in sample
+  n = 350000
+
+  
+  print('-------------------')
+  print(paste0("Number of covariates that affect selection: ", ncs, " of ", nc))
+  print(paste0("Correlation between covariates: ", corrC))
+  
+
+  ###
+  ### generate data
+  
+  ## generate covariates C with particular correlation corrC  
+  corrCMat = diag(nc)
+  corrCMat[which(corrCMat == 0)] = corrC
+  dfC = mvrnorm(n=n, mu=rep(0, nc), Sigma=corrCMat, empirical=FALSE)
+  dfC = as.data.frame(dfC)
+  
+  ## generate a IV with 3 categories
+  ## use p(A1)=0.8, p(A2)=0.2, dosage probs assuming HWE = (0.8^2, 2*0.8*0.2, 0.2^2) = (0.64,0.32,0.04) 
+  z = sample(1:3, n, replace=TRUE, prob=c(0.64, 0.32, 0.04))
+  
+
+  ###
+  ### calculate effects of covariates on X and Y, fixing the total effect of C_s and C_nots respectively.
+
+  betaCs_onX = log(2^(1/ncs))
+  betaCnots_onX = log(2^(1/(nc-ncs)))
+
+
+  ###
+  ### generate exposure x
+
+  logitPart = z + rowSums(betaCs_onX*dfC[,1:ncs, drop=FALSE]) + rowSums(betaCnots_onX*dfC[,(ncs+1):nc, drop=FALSE]) 
+  pX = exp(logitPart)/(1+exp(logitPart))
+  x = rep(0, 1, n)
+  x[runif(n) <= pX] = 1
+  
+
+  ###
+  ### generate continuous outcome y
+
+  # C AND X ARE DETERMINANTS OF Y
+
+  betaCs_onY = 2/ncs
+  betaCnots_onY = 2/(nc-ncs)
+
+  y = rowSums(betaCs_onY*dfC[,1:ncs, drop=FALSE]) + rowSums(betaCnots_onY*dfC[,(ncs+1):nc, drop=FALSE]) + 1*x + rnorm(n,0,1)
+
+
+
+  ###
+  ### generate selection variable s
+
+  # beta is set so that the total effect across all covariates affect d is constant
+  betaC = log(totalEffectCovarsSelection^(1/ncs))
+
+  # X AND (A SUBSET OF) C ARE DETERMINANTS OF SELECTION
+  logitPart = log(2)*x + rowSums(dfC[,1:ncs, drop=FALSE]*betaC) 
+  pS = exp(logitPart)/(1+exp(logitPart))
+  s = rep(0, 1, n)
+  s[runif(n) <= pS] = 1
+
+
+
+  ##
+  ##  combine into a dataframe
+
+
+  ###
+  ### select subsample
+  
+  simdata = list(x = x[which(s == 1)], y = y[which(s == 1)], z = z[which(s == 1)], dfC=dfC[which(s==1),])
+  
+
+  return(simdata)
+  
+}
