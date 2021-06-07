91ea2876871df149da83c7e3cb6a33b3a4624577
Louise Millard <louise.millard@bristol.ac.uk>
2021-06-07 08:51:05 +0100

    add selection sim jobs with nc and ncnots 2 versions

---
+++ 1-sims/b-selection-bias/jobs/jobs-10-2/j-10-2-rs0_8-r0_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=10
+#PBS -o out-10-2-corrC0_8-r0_2.file
+#---------------------------------------------
+
+date
+
+
+cd $PBS_O_WORKDIR
+cd ../..
+
+module add languages/R-4.0.3-gcc9.1.0
+
+
+export RES_DIR="${HOME}/2021-randomization-test/results"
+
+# ncs ncnots corrC rSelection
+Rscript sim-selection.R 10 2 0.8 0.2
+
+date
+
