18ed14cda66d993f706158655a9f6225289ec1be
Louise Millard <louise.millard@bristol.ac.uk>
2021-06-07 09:21:44 +0100

    job text changes

---
+++ 1-sims/b-selection-bias/jobs/jobs-2-50/j-2-50-rc0-r0_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=10
+#PBS -o out-2-50-rc0-r0_2.file
+#---------------------------------------------
+
+date
+
+
+cd $PBS_O_WORKDIR
+cd ../..
+
+module add languages/R-4.0.3-gcc9.1.0
+
+
+export RES_DIR="${HOME}/2021-randomization-test/results"
+
+# ncs ncnots rc rSelection
+Rscript sim-selection.R 2 50 0 0.2
+
+date
+
