e5c76cf3a4dadc9251f738f599b4ee076327c9b4
Louise Millard <louise.millard@bristol.ac.uk>
2021-07-22 12:40:57 +0100

    update selection sim to allow zero ncs and ncnots

--- 1-sims/b-selection-bias/generateContinuousX2.R
+++ 1-sims/b-selection-bias/generateContinuousX2.R
@@ -13,23 +13,53 @@ generateContinuousX2 <- function(dfC, z, ncs, ivRsq=0.1) {
   # number of covariates
   nc = ncol(dfC)
   n = nrow(dfC)
-
-  ##
-  ## generate intermediate outcomes
-
-  # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
-  tmpCS = rowSums(dfC[,1:ncs, drop=FALSE]) 
-  tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE]) 
-
-  # doesn't matter that they have different variance because we account for their variance in the next step
-  print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
-  print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
+  ncnots = nc - ncs
 
 
-  ##
-  ## continuous outcome y
+  if (ncs>0 & ncnots>0) {
+    print('generating X using Z, CS and CnotS')
 
-  x = combineDeterminants2(covars=data.frame(tmpCS=tmpCS, tmpCNOTS=tmpCNOTS), otherdet=z, varExplOtherDet=ivRsq, varExplCovars=0.1)
+    ## generate intermediate outcomes
+    # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
+    tmpCS = rowSums(dfC[,1:ncs, drop=FALSE]) 
+    tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE]) 
+
+    # doesn't matter that they have different variance because we account for their variance in the next step
+    print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
+    print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
+
+
+    ## continuous exposure X
+
+    x = combineDeterminants2(covars=data.frame(tmpCS=tmpCS, tmpCNOTS=tmpCNOTS), otherdet=z, varExplOtherDet=ivRsq, varExplCovars=0.1)
+
+  }
+  else if (ncs>0 & ncnots==0) {
+
+    print('generating X using Z, CS')
+
+    tmpCNOTS = NULL
+    tmpCS = rowSums(dfC[,1:ncs, drop=FALSE])
+    print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
+
+    ## continuous exposure X
+    x = combineDeterminants3(z, tmpCS, ivRsq, 0.1)
+
+  }
+  else if (ncs==0 & ncnots>0) {
+
+    print('generating X using Z, CSnotS')
+
+    tmpCS = NULL
+    tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE])
+    print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
+
+    ## continuous exposure X
+    x = combineDeterminants3(z, tmpCNOTS, ivRsq, 0.1)
+
+  }
+
+
 
   return(list(x=x, tmpCS=tmpCS, tmpCNOTS=tmpCNOTS))
 
--- 1-sims/b-selection-bias/generateContinuousY2.R
+++ 1-sims/b-selection-bias/generateContinuousY2.R
@@ -13,27 +13,59 @@ generateContinuousY2 <- function(dfC, x, ncs) {
   # number of covariates
   nc = ncol(dfC)
   n = nrow(dfC)
+  ncnots = nc - ncs
 
-  ##
-  ## generate intermediate outcomes
+  if (ncs>0 & ncnots>0) {
 
-  # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
-  tmpCS = rowSums(dfC[,1:ncs, drop=FALSE]) 
-  tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE]) 
+    ## generate intermediate outcomes
 
-  # doesn't matter that they have different variance because we account for their variance in the next step
-  print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
-  print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
+    # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
+    tmpCS = rowSums(dfC[,1:ncs, drop=FALSE]) 
+    tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE]) 
 
+    # doesn't matter that they have different variance because we account for their variance in the next step
+    print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
+    print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
 
+    tmps = combineDeterminants3(tmpCS, tmpCNOTS, 0.5, 0.5)
 
-  tmps = combineDeterminants3(tmpCS, tmpCNOTS, 0.5, 0.5)
+    ## continuous outcome y
+    y = combineDeterminants3(x, tmps, 0.3, 0.2)
 
+  }
 
-  ##
-  ## continuous outcome y
+  else if (ncs>0 & ncnots==0) {
 
-  y = combineDeterminants3(x, tmps, 0.3, 0.2)
+    ## generate intermediate outcomes
+
+    # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
+    tmpCS = rowSums(dfC[,1:ncs, drop=FALSE])
+    tmpCNOTS = NULL
+
+    # doesn't matter that they have different variance because we account for their variance in the next step
+    print(paste0('tmpCS intermed: mean=', mean(tmpCS), ', sd=', sd(tmpCS)))
+
+    tmps = tmpCS
+
+    ## continuous outcome y
+    y = combineDeterminants3(x, tmps, 0.3, 0.2)
+  }
+  else if (ncs==0 & ncnots>0) {
+    
+    ## generate intermediate outcomes
+
+    # combine the variables in CS and CNOTS INTO COMBINED VARIABLES, RESPECTIVELY.
+    tmpCS = NULL
+    tmpCNOTS = rowSums(dfC[,(ncs+1):nc, drop=FALSE])
+
+    # doesn't matter that they have different variance because we account for their variance in the next step
+    print(paste0('tmpCNOTS intermed: mean=', mean(tmpCNOTS), ', sd=', sd(tmpCNOTS)))
+
+    tmps = tmpCNOTS
+
+    ## continuous outcome y
+    y = combineDeterminants3(x, tmps, 0.3, 0.2)
+  }
 
   return(list(y=y, tmpCS=tmpCS, tmpCNOTS=tmpCNOTS, tmps=tmps))
 
