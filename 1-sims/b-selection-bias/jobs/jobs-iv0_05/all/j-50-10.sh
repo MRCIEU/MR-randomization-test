4f2c18c9fda255f74d44f29c12df5c47dc920944
Louise Millard <louise.millard@bristol.ac.uk>
2021-09-10 14:10:32 +0100

    jobs for no selection sim versions

---
+++ 1-sims/b-selection-bias/jobs/jobs-iv0_05/all/j-50-10.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -l walltime=120:00:00,nodes=1:ppn=10
+#PBS -o out-50-10.file
+#PBS -t 1-30
+#---------------------------------------------
+
+date
+
+
+cd $PBS_O_WORKDIR
+
+cd ../../../
+
+# get settings
+settingLine=${PBS_ARRAYID}
+
+rsqC=`cat jobs/settings.txt | sed -n ${settingLine}p | cut -d, -f1`
+rsqS=`cat jobs/settings.txt | sed -n ${settingLine}p | cut -d, -f2`
+covarsInc=`cat jobs/settings.txt | sed -n ${settingLine}p | cut -d, -f3`
+
+module add languages/R-4.0.3-gcc9.1.0
+
+
+export RES_DIR="${HOME}/2021-randomization-test/results"
+
+
+ncs=50
+ncnots=10
+ivEffect=0.05
+
+
+# ncs ncnots rc rSelection
+Rscript sim-selection.R $ncs $ncnots $rsqC $rsqS $ivEffect "grs" $covarsInc 1
+
+date
+
