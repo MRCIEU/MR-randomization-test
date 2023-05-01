de5fdda6c5ffc81f75fe1ff5537f3eeb93e047e4
louise <louise.millard@gmail.com>
2023-05-01 14:42:19 +0100

    update HP sim jobs to BC phase 4

--- 1-sims/c-horizontal-pleiotropy/jobs/jobs-iv0_05/j-1-1.sh
+++ 1-sims/c-horizontal-pleiotropy/jobs/jobs-iv0_05/j-1-1.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-#PBS -l walltime=120:00:00,nodes=4:ppn=16
-#PBS -o out-1-1.file
-#PBS -t 1-6
+
+#SBATCH --job-name=j1-1-0_001
+#SBATCH --partition=cpu
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=6-0:0:0
+#SBATCH --mem=100G
+#SBATCH --array=1-6
+#SBATCH --account=ACC1234
+
 #---------------------------------------------
 
 date
 
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
 
 cd ../../
 
 # get settings
-settingLine=${PBS_ARRAYID}
+settingLine=${SLURM_ARRAY_TASK_ID}
 
 rsqC=`cat jobs/settings-hp.txt | sed -n ${settingLine}p | cut -d, -f1`
 numhpSnps=`cat jobs/settings-hp.txt | sed -n ${settingLine}p | cut -d, -f2`
 numnonhpSnps=`cat jobs/settings-hp.txt | sed -n ${settingLine}p | cut -d, -f3`
 covarsInc=`cat jobs/settings-hp.txt | sed -n ${settingLine}p | cut -d, -f4`
 
-module add languages/R-4.0.3-gcc9.1.0
+module add languages/r/4.2.1
 
 
 export RES_DIR="${HOME}/2021-randomization-test/results"
